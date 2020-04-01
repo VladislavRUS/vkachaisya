7d7f1ccd79e68b2e74c55399bcb0794b4f8870ea
Vladislav Kurochkin <vlad555.95@mail.ru>
2020-04-01 22:17:44 +0400

    Fix url

--- vkachaisya-frontend/src/api/index.ts
+++ vkachaisya-frontend/src/api/index.ts
@@ -5,6 +5,6 @@ const API_CLIENT = axios.create();
 API_CLIENT.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
 API_CLIENT.defaults.headers['url'] =
-  process.env.NODE_ENV === 'development' ? process.env.REACT_APP_MOCK_URL : window.location.href;
+  process.env.NODE_ENV === 'development' ? process.env.REACT_APP_MOCK_URL : window.location.search.slice(1);
 
 export { API_CLIENT };
